5082469d0e57017bc4df293d27bcd8978901e427
ccccchhhheeenng <98439044+ccccchhhheeenng@users.noreply.github.com>
2022-11-16 22:38:41 +0800

    Update 基本判斷和$_.基礎.ps1

--- basic/基本判斷和$_.基礎.ps1
+++ basic/基本判斷和$_.基礎.ps1
@@ -6,10 +6,15 @@ if (Get-Process |Where-Object{($_.Name) -match $a}) {
         Write-Output "The process has been killed"
     }else{
         Get-Process |Where-Object{$_.Name -match $a}
-        Write-Output "Choose the process you want to kill"
+        Write-Output "Choose the process you want to kill,input a to kill all"
         $id=Read-Host -Prompt "pid"
+        if ($id -eq "a"){
+            Stop-Process -Name $av
+            Write-Output "All of the process has been killed"
+        }else{
         Stop-Process -id $id -Confirm
         Write-Output "The process has been killed"
+        }
     }
 }else{
     Write-Output "Process not found"
