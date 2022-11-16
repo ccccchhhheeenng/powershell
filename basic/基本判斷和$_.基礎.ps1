a4e84ea73403a934183caaa61f576da8b2bc12da
ccccchhhheeenng <98439044+ccccchhhheeenng@users.noreply.github.com>
2022-11-16 14:39:25 +0800

    Update 基本判斷和$_.基礎.ps1

--- basic/基本判斷和$_.基礎.ps1
+++ basic/基本判斷和$_.基礎.ps1
@@ -1,9 +1,16 @@
 Out-Null
-if (Get-Process |Where-Object{$_.Name -match "word"}) {
-    cmd /c "taskkill /im word.exe /f"
-    Write-Output "The process has been killed"
-}
-else {
-    <# Action when all if and elseif conditions are false #>
+$a=Read-Host -Prompt "processname"
+if (Get-Process |Where-Object{($_.Name) -match $a}) {
+    if((Get-Process |Where-Object{$_.Name -match $a}).count -eq 1){
+        Stop-Process -Name $a -Confirm
+        Write-Output "The process has been killed"
+    }else{
+        Get-Process |Where-Object{$_.Name -match "notepad"}
+        Write-Output "Choose the process youwant to kill"
+        $id=Read-Host -Prompt "pid"
+        Stop-Process -id $id -Confirm
+        Write-Output "The process has been killed"
+    }
+}else{
     Write-Output "process not found"
 }
